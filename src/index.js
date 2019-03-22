f100acdfbdc749971b6dc0a4086e15ed00c69dde
Nikita Zhavoronkov <larkgronick@gmail.com>
2019-03-22 10:25:30 +0300

    feat:load solution with 6 passing tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+let calculate = 0;
+for (let i = 5; i <= number; i += 5) {
+    let number = i;
+    while (number % 5 === 0) {
+      number /= 5;
+      calculate++;
+    }
+}
+return calculate;
 }
